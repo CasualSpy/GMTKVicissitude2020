5bcd809c68732b390ee3049e0efb9a8876631123
EBhero <etiennebrisson@hotmail.com>
2020-07-10 18:09:52 -0400

    Guests Shall now be summoned

---
+++ Assets/Scripts/GuestSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuestSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject GuestGameObject;
+
+    public int TargetQuantity = 10;
+    public float Rate = 1;
+
+    private float spawnTimer;
+
+    void Start()
+    {
+        spawnTimer = Rate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0)
+        {
+            SpawnLogic();
+            spawnTimer = Rate;
+        }
+
+    }
+
+    private void SpawnLogic()
+    {
+        if (GameObject.FindGameObjectsWithTag("Guest").Length < TargetQuantity)
+            Instantiate(GuestGameObject, new Vector3(-10, 0, 0), Quaternion.identity);
+    }
+}
