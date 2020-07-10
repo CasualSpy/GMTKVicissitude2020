3b57bed24986bbc4b9ea25142c815e0cada586f3
CasualSpy <arnaudbegin@hotmail.com>
2020-07-10 19:33:36 -0400

    Add complaints and helpers that might be useful later

---
+++ Assets/ComplaintManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ComplaintManager : MonoBehaviour
+{
+    public struct Complaint
+    {
+        public GuestMain guest;
+        public Reasons reason;
+    }
+
+    Dictionary<Reasons, int> complaintValues;
+    public List<Complaint> complaints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        complaints = new List<Complaint>();
+        complaintValues = new Dictionary<Reasons, int>();
+        complaintValues.Add(Reasons.Designated_driver, 2);
+        complaintValues.Add(Reasons.Unsafe_sex, 2);
+        complaintValues.Add(Reasons.Consent, 2);
+        complaintValues.Add(Reasons.Dirty_house, 1);
+        complaintValues.Add(Reasons.Kickout, 1);
+        complaintValues.Add(Reasons.Minor, 1);
+        complaintValues.Add(Reasons.Noise, 1);
+    }
+
+    public enum Reasons
+    {
+        Kickout,
+        Unsafe_sex,
+        Consent,
+        Designated_driver,
+        Minor,
+        Dirty_house,
+        Noise
+    }
+
+
+    public void AddComplaint(Complaint complaint)
+    {
+        complaints.Add(complaint);
+    }
+
+    public int TotalValue()
+    {
+        return complaints.Aggregate(0, (acc, x) => acc + complaintValues[x.reason]);
+    }
+}
---
+++ Assets/Helpers.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Helpers
+{
+    #region Guests 
+
+    public enum SkinColor
+    {
+        Pale,
+        Asian,
+        Tan,
+        Dark,
+        Black
+    }
+
+    public enum HairColor
+    {
+        Black,
+        DarkBrown,
+        LightBrown,
+        Blond,
+        Red
+    }
+
+    public static Color ShirtColor()
+    {
+        List<Color> colors = new List<Color>
+        {
+            Color.green, Color.red, Color.yellow, Color.cyan, Color.blue, Color.magenta
+        };
+
+        return colors[UnityEngine.Random.Range(0, colors.Count)];
+    }
+
+    public static SkinColor GetSkinColor()
+    {
+        var values = Enum.GetValues(typeof(SkinColor));
+        return (SkinColor)values.GetValue(UnityEngine.Random.Range(0, values.Length));
+    }
+
+    public static HairColor GetHairColor(SkinColor skinColor)
+    {
+        switch (skinColor)
+        {
+            case SkinColor.Black:
+            case SkinColor.Asian:
+            case SkinColor.Dark:
+                List<HairColor> colors = new List<HairColor> { HairColor.Black, HairColor.DarkBrown, HairColor.LightBrown };
+                return colors[UnityEngine.Random.Range(0, colors.Count)];
+            case SkinColor.Tan:
+                List<HairColor> colorsTan = new List<HairColor> { HairColor.Black, HairColor.DarkBrown, HairColor.LightBrown, HairColor.Blond };
+                return colorsTan[UnityEngine.Random.Range(0, colorsTan.Count)];
+            case SkinColor.Pale:
+                var colorsPale = Enum.GetValues(typeof(HairColor));
+                return (HairColor)colorsPale.GetValue(UnityEngine.Random.Range(0, colorsPale.Length));
+            default:
+                return HairColor.DarkBrown;
+        }
+    }
+    #endregion
+}
