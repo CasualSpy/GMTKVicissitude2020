87176a43fb75091991b44bd88a66a50a7414a6f4
CasualSpy <arnaudbegin@hotmail.com>
2020-07-10 17:21:23 -0400

    Add basic movement controls

---
+++ Assets/ControlsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsManager : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float movementVertical = Input.GetAxis("Vertical");
+        float movementHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(movementHorizontal, movementVertical);
+        rb2d.AddForce(movement * speed);
+    }
+}
