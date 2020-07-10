b3b85a371116beb919e5eb2fa5941ac1448eb1fc
EBhero <etiennebrisson@hotmail.com>
2020-07-10 19:02:16 -0400

    GuestMain

---
+++ Assets/Scripts/GuestMain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuestMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    AbsGuestBehavior guestBehavior;
+
+    enum State
+    {
+        Idle,
+        Leaving
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+}
