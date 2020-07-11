ae40065794537f6bcba7db50ff21eee80706f996
p-victor <poiriervictor99@gmail.com>
2020-07-11 18:20:31 -0400

    Revert "added tresholds for windows annoyance"
    This reverts commit 675146c738dc6e77db6e1523b244631a0de50a52.

--- Assets/Scripts/SoundLevel.cs
+++ Assets/Scripts/SoundLevel.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SoundLevel : MonoBehaviour, Plaintiff
 {
-    private SpriteRenderer windowRenderer;
     public float soundLevel;
     public float annoyance;
     public float annoyanceSpeed;
@@ -15,13 +14,12 @@ public class SoundLevel : MonoBehaviour, Plaintiff
     {
         soundLevel = 0f;
         annoyance = 0f;
-        windowRenderer = GameObject.Find("Window").GetComponent<SpriteRenderer>();
         cm = GameObject.Find("Master").GetComponent<ComplaintManager>();
     }
 
     public void Increase()
     {
-        soundLevel += soundLevel < 5f ? 1f : 0f;
+        soundLevel += soundLevel < 1f ? 0.1f : 0f;
     }
 
     public void Lower()
@@ -32,25 +30,7 @@ public class SoundLevel : MonoBehaviour, Plaintiff
     private void FixedUpdate()
     {
         annoyance += soundLevel * Time.fixedDeltaTime * annoyanceSpeed;
-        if (annoyance == 0f)
-        {
-            windowRenderer.sprite = null;
-        }
-        else if (annoyance >= 2f)
-        {
-
-        }
-        else if (annoyance >= 4f)
-        {
-        }
-        else if (annoyance >= 6f)
-        {
-        }
-        else if (annoyance >= 8f)
-        {
-
-        }
-        else if (annoyance >= 10f)
+        if (annoyance >= 10f)
         {
             cm.AddComplaint(new ComplaintManager.Complaint() { plaintiff = this, reason = ComplaintManager.Reasons.Noise }) ;
             annoyance = 0f;
